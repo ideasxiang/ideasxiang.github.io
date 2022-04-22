948fc6c0f9e2fcb5be3c49d801c9dc297c7b9e45
Ren Xiang <ideasxiang@gmail.com>
2022-04-22 12:22:29 +0800

    fixed scaling

--- script.js
+++ script.js
@@ -77,14 +77,18 @@ function renderPlaces(places) {
     let scene = document.querySelector('a-marker');
 
     places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
         let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
         setModel(models[modelIndex], model);
 
         model.setAttribute('animation-mixer', '');
 
         document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gltf-model]');
+            var entity = document.querySelector('[gps-entity-place]');
             modelIndex++;
             var newIndex = modelIndex % models.length;
             setModel(models[newIndex], entity);
